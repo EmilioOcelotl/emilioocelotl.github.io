dbd75e1f0fb664ed4331e385fb382c8b5cd8c2d8
EmilioOcelotl <emilio.ocelotl@gmail.com>
2024-06-04 12:03:26 -0600

    organizacion y entradas

--- src/main.js
+++
@@ -1,7 +0,0 @@
-function redirectToProject(projectUrl) {
-    window.location.href = projectUrl;
-  }
-  
-  function redirectToPublication(publicationUrl) {
-    window.location.href = publicationUrl;
-  }  
