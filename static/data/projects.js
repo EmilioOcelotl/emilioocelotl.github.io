047d42d22954fc2672161c1614a29979a486179d
EmilioOcelotl <emilio.ocelotl@gmail.com>
2024-06-04 21:12:12 -0600

    descripciones e imagenes

--- static/data/projects.js
+++ static/data/projects.js
@@ -1,31 +1,15 @@
 export const pprojects = [
   {
     "href": "src/tres.html",
-    "imgSrc": "./img/tres.png",
+    "imgSrc": "./img/tres.jpg",
     "imgAlt": "TRES ESTUDIOS ABIERTOS",
     "title": "Tres Estudios Abiertos",
     "year": "2024",
     "description": "Escritura, JavaScript e investigación artística (en curso)."
   },
   {
-    "href": "nr.html",
-    "imgSrc": "./img/nucleoroto.jpg",
-    "imgAlt": "nr2024",
-    "title": "Future Sound of Nucleoroto Sesión 1",
-    "year": "2024",
-    "description": "Experiencia audiovisual electrónica experimental."
-  },
-  {
-    "href": "codepunk.html",
-    "imgSrc": "./img/2.png",
-    "imgAlt": "codepunk",
-    "title": "code punk alchemy",
-    "year": "2024",
-    "description": "live coding visuals, music"
-  },
-  {
     "href": "stanford2024.html",
-    "imgSrc": "./img/1.jpeg",
+    "imgSrc": "./img/residencia.jpg",
     "imgAlt": "stanford2024",
     "title": "Audio-Visual Sessions",
     "year": "2024",
@@ -37,23 +21,23 @@ export const pprojects = [
     "imgAlt": "nime2023",
     "title": "NIME 2023",
     "year": "2023",
-    "description": "Presentacion A/V en NIME junto con M. Teixido"
+    "description": "Presentacion A/V en NIME junto con Marianne Teixido"
   },
   {
     "href": "algorave2023.html",
-    "imgSrc": "./img/algorave.jpeg",
+    "imgSrc": "./img/algorave2.jpg",
     "imgAlt": "algorave",
     "title": "Algorave CDMX",
     "year": "2023",
-    "description": ""
+    "description": "Colaboración con Dorian Sotomayor"
   },
   {
-    "href": "cusco.html",
-    "imgSrc": "./img/cusco.jpg",
-    "imgAlt": "cusco2022",
-    "title": "Concierto A/V",
+    "href": "xicalco.html",
+    "imgSrc": "./img/xicalco.jpeg",
+    "imgAlt": "xicalco2022",
+    "title": "Xicalcoliuhqui",
     "year": "2022",
-    "description": "PUMPUMYACHKAN Festival Asimtria"
+    "description": "Presentación de RGGTRN con Gabriel de Dios El Hijo de Tere"
   },
   {
     "href": "anti.html",
@@ -65,10 +49,50 @@ export const pprojects = [
   },
   {
     "href": "three.html",
-    "imgSrc": "./img/threeNw.png",
+    "imgSrc": "./img/iracema.jpg",
     "imgAlt": "three2022",
     "title": "THREE.studies",
     "year": "2022",
     "description": "Estudios A/V para el navegador"
+  },
+  {
+    "href": "cusco.html",
+    "imgSrc": "./img/cusco.jpg",
+    "imgAlt": "cusco2022",
+    "title": "Concierto A/V",
+    "year": "2021",
+    "description": "PUMPUMYACHKAN Festival Asimtria"
+  },
+  {
+    "href": "feedback.html",
+    "imgSrc": "./img/of13.jpg",
+    "imgAlt": "feedback",
+    "title": "Feedback",
+    "year": "2020",
+    "description": "Exploraciones entre código, sonido e imagen"
+  },
+  {
+    "href": "noche.html",
+    "imgSrc": "./img/data.jpg",
+    "imgAlt": "nocheDeLasEstrellas",
+    "title": "Noche de las Estrellas",
+    "year": "2019",
+    "description": "Intervención estética de datos astronómicos"
+  },
+  {
+    "href": "linux.html",
+    "imgSrc": "./img/lac.jpg",
+    "imgAlt": "lac",
+    "title": "Linux Audio Conference",
+    "year": "2019",
+    "description": "Presentación de RGGTRN en Linux Audio Conference"
+  },
+  {
+    "href": "algoraveccd.html",
+    "imgSrc": "./img/algoraveccd2.jpg",
+    "imgAlt": "ccdAlgorave",
+    "title": "Centro de Cultura Digital",
+    "year": "2018",
+    "description": "2/4 RGGTRN (M. Teixido + E. Ocelotl) Algorave en CCD"
   }
 ];
