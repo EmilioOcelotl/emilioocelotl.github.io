fd529ec0dfe19efcf769b2d7ac9c133066a62edf
EmilioOcelotl <emilio.ocelotl@gmail.com>
2025-01-07 19:32:32 -0600

    tres tres

--- static/data/projects.js
+++ static/data/projects.js
@@ -1,22 +1,55 @@
 export const projects = [
   {
     "href": "src/tres.html",
-    "imgSrc": "./img/tres2.jpg",
+    "imgSrc": "./img/tres2-r.jpg",
     "imgAlt": "TRES ESTUDIOS ABIERTOS",
     "title": "Tres Estudios Abiertos",
-    "year": "2024",
+    "year": "2020-2024",
     "description": "Escritura, JavaScript e investigación artística (en curso).",
     "details": {
       "fullDescription": "<p>Tres Estudios Abiertos tiene lugar en una frontera donde el conocimiento y lo sensible se encuentran, se adscribe a la <emph>generación de nuevos modos de performance musical y a la apertura de canales innovadores para la presentación y difusión de la investigación artística en música</emph> (de Assis, 2018).</p><p>En este sentido surge la pregunta presente en el núcleo de esta investigación: ¿Qué aportes puede realizar la escritura de código a los nuevos modos del performance audiovisual y a la escritura de un documento reflexivo que involucra materiales como sonido, gráficos y texto en el contexto de la investigación artística?</p><p>La respuesta a esta pregunta parte del lenguaje de programación JavaScript. El resultado publicado como página web se puede consultar en: <a href='https://tres.ocelotl.cc/'>tres.ocelotl.cc</a>. Este proyecto está complementado con un repositorio que gestiona el trasfondo y que imprime el resultado de esta investigación como un archivo PDF jerarquizado.</p><p>Código del proyecto disponible en: <a href='https://github.com/EmilioOcelotl/tres-front'>https://github.com/EmilioOcelotl/tres-front</a> y en <a href='https://github.com/EmilioOcelotl/tres-back'>https://github.com/EmilioOcelotl/tres-back</a></p><h3>Referencias</h3><p>De Assis, P. (2018). Logic of Experimentation. Leuven University Press. <a href='https://doi.org/10.2307/j.ctv6zdcpg'>https://doi.org/10.2307/j.ctv6zdcpg</a></p>",
       "images": [
-        "./img/tres.jpg",
-        "./img/tres2.jpg"
+        "./img/tres-r.jpg",
+        "./img/tres2-r.jpg"
+      ]
+    }
+  },
+  {
+    "href": "three.html",
+    "imgSrc": "./img/three2024-2.jpg",
+    "imgAlt": "three2022",
+    "title": "THREE.studies",
+    "year": "2022-2024",
+    "description": "Estudios A/V para el navegador",
+    "details": {
+      "fullDescription": "<p>Estudios audiovisuales para el navegador. Este proyecto fue realizado en colaboración con Iracema de Andrade.</p><p>El repositorio con las primeras versiones se encuentra en: <a href= 'https://github.com/EmilioOcelotl/THREE.studies'>THREE.studies</a>.</p><p>La última versión se encuentra respaldada en: <a href='https://github.com/EmilioOcelotl/THREE.studies-II'>THREE.studies-II</a>. Para esta versión, despliego la instancia de un <a href='https://github.com/EmilioOcelotl/osc-web-server'>servidor osc web personalizado</a> y la biblioteca <a href='https://github.com/EmilioOcelotl/treslib'>treslib</a>, ambas escritas por mí. También implementa una <a href='https://github.com/EmilioOcelotl/MMLL'>variación</a> de la biblioteca MMLL.js trabajada en conjunto con Marianne Teixido, <a href='https://github.com/sicklincoln/MMLL'>proyecto original</a> de Nick Collins.</p>",
+      "images": [
+        "./img/iracema-r.jpg",
+        "./img/threeclnF-r.png",
+        "./img/three2024.jpg",
+        "./img/threeNw-r.png"
+      ]
+    }
+  },
+  {
+    "href": "anti.html",
+    "imgSrc": "./img/antiHydra2-r.jpg",
+    "imgAlt": "anti2022",
+    "title": "Anti",
+    "year": "2022-2024",
+    "description": "Ofuscación audiovisual en el navegador.",
+    "details": {
+      "fullDescription": "<p>La ofuscación como motivo, la escritura como rodeo.</p><p>Anti es un llamado a la responsabilidad de los datos, al compromiso y el cuidado y a la realización de usuarixs que desdibujan las fronteras de la pasividad política y económica teniendo como epicentro lo sensible.</p><p><a href='https://anti.ocelotl.cc/'>https://anti.ocelotl.cc/</a></p><p>Código del proyecto: <a href='https://github.com/EmilioOcelotl/anti'>https://github.com/EmilioOcelotl/anti</a></p><p>Proyecto apoyado por el Fondo Nacional para la Cultura y las Artes</p>",
+      "images": [
+        "./img/anti2-r.jpg",
+        "./img/anti-r.jpg",
+        "./img/antiHydra2-r.jpg"
       ]
     }
   },
   {
     "href": "stanford2024.html",
-    "imgSrc": "./img/residencia.jpg",
+    "imgSrc": "./img/residencia-r.jpg",
     "imgAlt": "stanford2024",
     "title": "Audio-Visual Sessions",
     "year": "2024",
@@ -24,17 +57,17 @@ export const projects = [
     "details": {
       "fullDescription": "<p>Residencia artística realizada en Center for Computer Research in Music and Acoustics (CCRMA), Stanford junto con Marianne Teixido del 8 al 17 de enero de 2024.</p><p>Durante nuestra estancia trabajamos con 6 estudiantes de composición y presentamos dos conciertos: uno en CCRMA Stage y otro en Elliott Program Center.</p><p>Los estudiantes con los que colaboramos fueron: Celeste Betancur, Brian Brown, Tatiana Catanzaro, Anna Golubkova, Lemon Guo, Nick Shaheed.</p>",
       "images": [
-        "./img/residencia.jpg",
-        "./img/ccrma2024-1.jpg",
-        "./img/ccrmaCel.jpg",
-        "./img/sd.jpeg"
+        "./img/residencia-r.jpg",
+        "./img/ccrma2024-1-r.jpg",
+        "./img/ccrmaCel-r.jpg",
+        "./img/sd-r.jpeg"
       ],
       "videoEmbed": "<iframe width='560' height='315' src='https://www.youtube.com/embed/fjBn5qU85QI?si=QpEU2ODarxtDuPZE' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>"
     }
   },
   {
     "href": "nime2023.html",
-    "imgSrc": "./img/nime1.jpg",
+    "imgSrc": "./img/nime1-r.jpg",
     "imgAlt": "nime2023",
     "title": "NIME",
     "year": "2023",
@@ -42,15 +75,15 @@ export const projects = [
     "details": {
       "fullDescription": "<p>Presentación A/V en New Interfaces for Musical Expresion 2023.</p><p>La presentación se realizó en la explanada del Centro de Cultura Digital y se presentó en un programa con varias propuestas musicales y audiovisuales-</p><p>La organización de NIME decidió otorgarnos el premio a mejor música. El link con la decisión se puede encontrar en: <a href='https://www.nime2023.org/awards-photos'>https://www.nime2023.org/awards-photos</a>.</p>",
       "images": [
-        "./img/nime1.jpg",
-        "./img/nime3.jpg",
-        "./img/nime.jpg"
+        "./img/nime1-r.jpg",
+        "./img/nime3-r.jpg",
+        "./img/nime-r.jpg"
       ]
     }
   },
   {
     "href": "algorave2023.html",
-    "imgSrc": "./img/algorave2.jpg",
+    "imgSrc": "./img/algorave2-r.jpg",
     "imgAlt": "algorave",
     "title": "Algorave CDMX",
     "year": "2023",
@@ -58,25 +91,25 @@ export const projects = [
     "details": {
       "fullDescription": "Presentación A/V con Dorian Sotomayor en Algorave CDMX 2023.",
       "images": [
-        "./img/algorave2.jpg"
+        "./img/algorave2-r.jpg"
       ],
       "videoEmbed": "<iframe width='560' height='315' src='https://www.youtube.com/embed/-2E_Hc4lCBw?si=Z6cXaSI4IdL5RZwv' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>"
     }
   },
   {
     "href": "xicalco.html",
-    "imgSrc": "./img/xicalco.jpeg",
+    "imgSrc": "./img/xicalco-r.jpeg",
     "imgAlt": "xicalco2022",
     "title": "Xicalcoliuhqui",
     "year": "2022",
-    "description": "Presentación de RGGTRN y Gabriel de Dios 'El Hijo de Tere'",
+    "description": "RGGTRN + Gabriel de Dios 'El Hijo de Tere'",
     "details": {
       "fullDescription": "RGGTRN full band en colaboración con Gabriel de Dios 'El Hijo de Tere' en Centro Cultural España de México (CDMX). ",
       "images": [
-        "./img/xicalco.jpeg",
-        "./img/2.jpeg",
-        "./img/3.jpeg",
-        "./img/6.jpeg",
+        "./img/xicalco-r.jpeg",
+        "./img/2-r.jpeg",
+        "./img/3-r.jpeg",
+        "./img/6-r.jpeg",
 
       ],
       "audioSrc": [
@@ -87,32 +120,8 @@ export const projects = [
         }
   },
   {
-    "href": "anti.html",
-    "imgSrc": "./img/antiHydra2.jpg",
-    "imgAlt": "anti2022",
-    "title": "Anti",
-    "year": "2022",
-    "description": "Ofuscación audiovisual en el navegador.",
-    "details": {
-      "fullDescription": "<p>La ofuscación como motivo, la escritura como rodeo.</p><p>Anti es un llamado a la responsabilidad de los datos, al compromiso y el cuidado y a la realización de usuarixs que desdibujan las fronteras de la pasividad política y económica teniendo como epicentro lo sensible.</p><p><a href='https://anti.ocelotl.cc/'>https://anti.ocelotl.cc/</a></p><p>Código del proyecto: <a href='https://github.com/EmilioOcelotl/anti'>https://github.com/EmilioOcelotl/anti</a></p><p>Proyecto apoyado por el Fondo Nacional para la Cultura y las Artes</p>",
-      "images": [
-        "./img/anti2.jpg",
-        "./img/anti.jpg",
-        "./img/antiHydra2.jpg"
-      ]
-    }
-  },
-  {
-    "href": "three.html",
-    "imgSrc": "./img/iracema.jpg",
-    "imgAlt": "three2022",
-    "title": "THREE.studies",
-    "year": "2022",
-    "description": "Estudios A/V para el navegador"
-  },
-  {
     "href": "cusco.html",
-    "imgSrc": "./img/cusco.jpg",
+    "imgSrc": "./img/cusco-r.jpg",
     "imgAlt": "cusco2022",
     "title": "Concierto A/V",
     "year": "2021",
@@ -120,14 +129,22 @@ export const projects = [
     "details": {
       "fullDescription": "Presentación A/V en Cusco, Perú en el marco de PUMPUMYACHKAN Festival Asimtria",
       "images": [
-        "./img/cusco.jpg"
+        "./img/cusco-r.jpg"
       ],
       "videoEmbed": "<iframe width='560' height='315' src='https://www.youtube.com/embed/XbRZuhifAJE?si=ygXWNT5CQiHuLbM5' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>"
     }
   },
   {
+    "href": "panorama-edges.html",
+    "imgSrc": "./img/edges-r.png",
+    "imgAlt": "edges",
+    "title": "Panorama-EDGES",
+    "year": "2020",
+    "description": "Espacios virtuales compartidos"
+  },
+  {
     "href": "feedback.html",
-    "imgSrc": "./img/of13.jpg",
+    "imgSrc": "./img/of13-r.jpg",
     "imgAlt": "feedback",
     "title": "Feedback",
     "year": "2020",
@@ -135,15 +152,15 @@ export const projects = [
   },
   {
     "href": "noche.html",
-    "imgSrc": "./img/disco3.jpg",
-    "imgAlt": "nocheDeLasEstrellas",
+    "imgSrc": "./img/disco3-r.jpg",
+    "imgAlt": "nocheDeLasEstrellas II",
     "title": "Noche de las Estrellas",
     "year": "2019",
     "description": "Intervención estética de datos astronómicos"
   },
   {
     "href": "linux.html",
-    "imgSrc": "./img/lac.jpg",
+    "imgSrc": "./img/lac-r.jpg",
     "imgAlt": "lac",
     "title": "Linux Audio Conference",
     "year": "2019",
@@ -151,7 +168,7 @@ export const projects = [
   },
   {
     "href": "algoraveccd.html",
-    "imgSrc": "./img/algoraveccd2.jpg",
+    "imgSrc": "./img/algoraveccd2-r.jpg",
     "imgAlt": "ccdAlgorave",
     "title": "Centro de Cultura Digital",
     "year": "2018",
@@ -159,15 +176,15 @@ export const projects = [
   },
   {
     "href": "ofhawc.html",
-    "imgSrc": "./img/ofhawc.jpg",
+    "imgSrc": "./img/ofhawc-r.jpg",
     "imgAlt": "ofhawc",
-    "title": "ofHAWC",
+    "title": "Noche de las Estrellas I - ofHAWC",
     "year": "2018",
     "description": "Visualización de datos astronómicos en la Noche de las Estrellas"
   },
   {
     "href": "lcne.html",
-    "imgSrc": "./img/lcne.jpg",
+    "imgSrc": "./img/lcne-r.jpg",
     "imgAlt": "iclc2017",
     "title": "Live CodeNet Ensamble",
     "year": "2017",
@@ -175,10 +192,26 @@ export const projects = [
   },
   {
     "href": "leviathan.html",
-    "imgSrc": "./img/levAle2.jpg",
+    "imgSrc": "./img/levAle2-r.jpg",
     "imgAlt": "leviathan2017",
     "title": "Leviathan",
     "year": "2017",
     "description": "Presentación de Leviathan en ISEA 2017 (Manizales, Colombia)"
+  },
+  {
+    "href": "altamisa.html",
+    "imgSrc": "./img/altamisa2-r.jpg",
+    "imgAlt": "altamisa",
+    "title": "Altamisa",
+    "year": "2016-2018",
+    "description": "Cello, electrónica e imagen"
+  },
+  {
+    "href": "rggtrn-bellacode.html",
+    "imgSrc": "./img/peru-r.jpg",
+    "imgAlt": "rggtrn-bellacode",
+    "title": "RGGTRN-Bellacode Tour",
+    "year": "2017",
+    "description": "RGGTRN - Bellacode Tour"
   }
 ];
