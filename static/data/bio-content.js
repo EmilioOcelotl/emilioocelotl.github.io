b952534434ca69852ba001b5812719c9d9c2f021
EmilioOcelotl <emilio.ocelotl@gmail.com>
2026-05-09 13:59:33 -0600

    color de tarjetas y fondo neutralizados

--- static/data/bio-content.js
+++ static/data/bio-content.js
@@ -15,11 +15,11 @@ export const bioContent = {
       works: "Obra y presentaciones seleccionadas",
       worksList: [
         "Audio-Visual Sessions. Presentación de piezas fijas creadas durante la residencia de RGGTRN (Marianne Teixido y Emilio Ocelotl). Con obras de: Celeste Betancur, Brian Brown, Tatiana Catanzaro, Anna Golubkova, Lemon Guo, Nick Shaheed. 19 de enero de 2024 en CCRMA Stage, CCRMA, Stanford.",
-        "Deep Structures. Concierto audiovisual en colaboración con Marianne Teixido. Presentación en NIME CDMX en Centro de Cultura Digital. Ciudad de México. 2023. Presentación en Sala Blas Galindo del Centro Nacional de las Artes en Ciudad de México. 2023",
+        "Deep Structures. Concierto audiovisual en colaboración con Marianne Teixido. Presentación en NIME CDMX en Centro de Cultura Digital. Ciudad de México. 2023. Presentación en Sala Blas Galindo del Centro Nacional de las Artes en Ciudad de México. 2023.",
         "Presentación en el festival .collect() realizado por TOPLAP Barcelona y Hangar. Barcelona, España. 7 a 9 de julio de 2022.",
         "THREE.studies. Presentación en el marco del programa Música de Cámara Música UNAM. Sala Carlos Chávez del CCU. Interpretación: Ana Karen Rodríguez. UNAM, Ciudad de México. (18 de junio de 2022). Presentación de THREE.studies en el marco del ciclo de conciertos Redirecciones Audiovisuales. Interpretación: Iracema de Andrade CMMAS. Morelia, México (25 de abril de 2022). Presentación de THREE.studies en el programa online del festival BEAST FEaST. Universidad de Birmingham, Reino Unido (22 a 24 de abril de 2021).",
         "Anti. Presentación en la muestra 'Creación en Movimiento' (segundo periodo) del programa de becarios del FONCA Jóvenes Creadores. Colegio de San Ildefonso. Ciudad de México (10 de febrero a 27 de marzo de 2022).",
-        "Presentación en el Festival Pumpumyachkan 2021 realizado por Asimetría. Cusco, Perú. 29 de noviembre a 3 de diciembre de 2021",
+        "Presentación en el Festival Pumpumyachkan 2021 realizado por Asimetría. Cusco, Perú. 29 de noviembre a 3 de diciembre de 2021.",
         "Presentación de fiestapirata.com en co-participación con RGGTRN en el marco de Linux Audio Conference. CCRMA, Stanford. 23 a 26 de marzo de 2019.",
         "Bellacode Tour con RGGTRN (Emilio Ocelotl Reyes, Marianne Teixido, Luis Navarro del Ángel y Jessica Rodríguez) Ciudad de México (MX), Bogotá, Manizales y Medellín (CO), Lima (PE) y Quito (EC).",
         "Altamisa. Ejecución para violoncello, electrónica y video. Violoncello, Iracema de Andrade; electrónica en vivo, Emilio Ocelotl Reyes. Foro Internacional de Música Nueva Manuel Enríquez (15 de febrero de 2018). Fonoteca Nacional, México (22 de junio de 2017). Centro Multimedia del Centro Nacional de las Artes, México (2 de junio de 2017). Latitudes Sonoras, MUAC. Ciudad de México. México (12 y 13 de noviembre de 2016). Festival Internacional Cervantino en colaboración con el Centro Mexicano para la Música y Las Artes Sonoras. Museo Iconográfico del Quijote. Guanajuato, Guanajuato. México (12 de octubre de 2016).",
@@ -31,7 +31,7 @@ export const bioContent = {
       ],
       teaching: "Docencia",
       teachingList: [
-        "Diseño de experiencias interdisciplinarias e interactivas. Tecnológico de Monterrey. Docente. 2025",
+        "Diseño de experiencias interdisciplinarias e interactivas. Tecnológico de Monterrey. Docente. 2025.",
         "Código Creativo, Narrativa Sonora, Ética de la Tecnología y Tecnología e Iluminación. CENTRO. CDMX. Docente. 2019-2025.",
         "Oferta Curricular Interdisciplinaria. CENART. CDMX. Docente. 2023-II y 2024-II.",
         "Seminario Permanente de Tecnología Musical. Posgrado en Música UNAM. CDMX. Coordinador. Semestre 2023-I."
@@ -44,9 +44,9 @@ export const bioContent = {
       ],
       publications: "Publicaciones",
       publicationsList: [
-        "Panorama. Escritura de espacios libres e inmersivos para el performance audiovisual en <a href='https://www.repositorio.fam.unam.mx/handle/123456789/139'>Algoritmos Arruinados. Perspectivas situadas de tecnología musical.</a> Escrito por E. Ocelotl, D. Sotomayor y M. Teixido. Publicado por UNAM. 2022",
+        "Panorama. Escritura de espacios libres e inmersivos para el performance audiovisual en <a href='https://www.repositorio.fam.unam.mx/handle/123456789/139'>Algoritmos Arruinados. Perspectivas situadas de tecnología musical.</a> Escrito por E. Ocelotl, D. Sotomayor y M. Teixido. Publicado por UNAM. 2022.",
         "<a href='https://zenodo.org/record/3715410#.X_4RjXWYVhE'>Aproximaciones al término Electroacústica en México.</a> Presentada en el Electroacoustic Music Studies Network (EMS). Franco, A., Dávila, R. y Ocelotl, E. Ciudad de México.",
-        "<a href='https://zenodo.org/records/4091446'>Bellacode: localized textual interfaces for live coding music.</a> CO-AUTOR. Madrid, España. Enero de 2019. Aceptado y publicado en las memorias de la Conferencia Internacional de Live Coding. Escrito por L. N. del Ángel, D. Ogborn, E. Ocelotl y M. Teixido",
+        "<a href='https://zenodo.org/records/4091446'>Bellacode: localized textual interfaces for live coding music.</a> CO-AUTOR. Madrid, España. Enero de 2019. Aceptado y publicado en las memorias de la Conferencia Internacional de Live Coding. Escrito por L. N. del Ángel, D. Ogborn, E. Ocelotl y M. Teixido.",
         "<a href='https://dj.dancecult.net/index.php/dancecult/article/view/1066'>Saborítmico: A report from the dance floor in Mexico.</a> CO-AUTOR. Online. Noviembre de 2018. Aceptado y publicado en Dancecult: Journal of Electronic Dance Music Culture. DOI del artículo: <a href='http://dx.doi.org/10.12801/1947-5403.2018.10.01.11'>doi</a>. Escrito por E. Ocelotl, M. Teixido y L. N. del Ángel. Febrero de 2021.",
         "<a href='https://drive.google.com/file/d/1haihAd45sRI_Ef_XZYsDz1GKORvjNANx/view'>Pull push. Flujos de trabajo distribuidos y no lineales en el performance, la producción y la investigación de sistemas audiovisuales interactivos.</a> Manizales, Colombia. AUTOR. Mayo de 2018. Aceptado y publicado en las memorias del Festival Internacional de la Imagen."
       ],
@@ -58,22 +58,22 @@ export const bioContent = {
       trajectory: "Trajectory",
       awards: "Awards, Distinctions and Residencies",
       awardsList: [
-        "Artistic residency at Center for Computer Research in Music and Acoustics. Stanford, California. 2024",
+        "Artistic residency at Center for Computer Research in Music and Acoustics. Stanford, California. 2024.",
         "Best Music Award at New Interfaces for Musical Expression (NIME). Mexico City 2023.",
-        "TOPLAP Barcelona, Hangar. Barcelona, Spain. July 2022",
+        "TOPLAP Barcelona, Hangar. Barcelona, Spain. July 2022.",
         "Platohedro. Medellín, Colombia. February 2017.",
         "Databit.me Festival. Arles, France. October 2014."
       ],
       works: "Selected Works and Presentations",
       worksList: [
         "Audio-Visual Sessions. Presentation of fixed pieces created during the RGGTRN residency (Marianne Teixido and Emilio Ocelotl). With works by: Celeste Betancur, Brian Brown, Tatiana Catanzaro, Anna Golubkova, Lemon Guo, Nick Shaheed. January 19, 2024 at CCRMA Stage, CCRMA, Stanford.",
-        "Deep Structures. Audiovisual concert in collaboration with Marianne Teixido. Presentation at NIME CDMX at Centro de Cultura Digital. Mexico City. 2023. Presentation at Sala Blas Galindo of the National Center for the Arts in Mexico City. 2023",
+        "Deep Structures. Audiovisual concert in collaboration with Marianne Teixido. Presentation at NIME CDMX at Centro de Cultura Digital. Mexico City. 2023. Presentation at Sala Blas Galindo of the National Center for the Arts in Mexico City. 2023.",
         "Presentation at the .collect() festival organized by TOPLAP Barcelona and Hangar. Barcelona, Spain. July 7-9, 2022.",
         "THREE.studies. Presentation as part of the Chamber Music program Música UNAM. Sala Carlos Chávez at CCU. Performance: Ana Karen Rodríguez. UNAM, Mexico City. (June 18, 2022). Presentation of THREE.studies as part of the Redirecciones Audiovisuales concert series. Performance: Iracema de Andrade CMMAS. Morelia, Mexico (April 25, 2022). Presentation of THREE.studies in the online program of the BEAST FEaST festival. University of Birmingham, UK (April 22-24, 2021).",
         "Anti. Presentation in the 'Creación en Movimiento' exhibition (second period) of the FONCA Young Creators fellowship program. Colegio de San Ildefonso. Mexico City (February 10 to March 27, 2022).",
-        "Presentation at the Pumpumyachkan Festival 2021 organized by Asimetría. Cusco, Peru. November 29 to December 3, 2021",
+        "Presentation at the Pumpumyachkan Festival 2021 organized by Asimetría. Cusco, Peru. November 29 to December 3, 2021.",
         "Presentation of fiestapirata.com in co-participation with RGGTRN as part of the Linux Audio Conference. CCRMA, Stanford. March 23-26, 2019.",
-        "Bellacode Tour with RGGTRN (Emilio Ocelotl Reyes, Marianne Teixido, Luis Navarro del Ángel and Jessica Rodríguez) Mexico City (MX), Bogotá, Manizales and Medellín (CO), Lima (PE) and Quito (EC).",
+        "Bellacode Tour with RGGTRN (Emilio Ocelotl Reyes, Marianne Teixido, Luis Navarro del Ángel and Jessica Rodríguez). Mexico City (MX), Bogotá, Manizales and Medellín (CO), Lima (PE) and Quito (EC).",
         "Altamisa. Performance for cello, electronics and video. Cello, Iracema de Andrade; live electronics, Emilio Ocelotl Reyes. International Forum of New Music Manuel Enríquez (February 15, 2018). Fonoteca Nacional, Mexico (June 22, 2017). Multimedia Center of the National Center for the Arts, Mexico (June 2, 2017). Latitudes Sonoras, MUAC. Mexico City, Mexico (November 12-13, 2016). International Cervantino Festival in collaboration with the Mexican Center for Music and Sound Arts. Museo Iconográfico del Quijote. Guanajuato, Guanajuato. Mexico (October 12, 2016).",
         "Leviathan. Performance for Paetzold flute, electronics and video. Paetzold flute, Alejandro Brianza; live electronics, Emilio Ocelotl Reyes and live video, Jessica Rodríguez. International Symposium on Electronic Art ISEA 2017 and 16th International Image Festival. University of Caldas. Manizales, Colombia. June 15, 2017.",
         "Algorave UK Tour with the RGGTRN collective (Emilio Ocelotl Reyes, Luis Navarro del Ángel and Jessica Rodríguez). Leeds (ODI Leeds), Newcastle (Abject Gallery), Hull (The New Adelphi Club and University of Hull) and London (Ravensbourne). United Kingdom. April 29 to May 6, 2016.",
@@ -83,7 +83,7 @@ export const bioContent = {
       ],
       teaching: "Teaching",
       teachingList: [
-        "Design of interdisciplinary and interactive experiences. Tecnológico de Monterrey. Professor. 2025",
+        "Design of interdisciplinary and interactive experiences. Tecnológico de Monterrey. Professor. 2025.",
         "Creative Code, Sound Narrative, Technology Ethics and Technology and Lighting. CENTRO. CDMX. Professor. 2019-2025.",
         "Interdisciplinary Curriculum Offer. CENART. CDMX. Professor. 2023-II and 2024-II.",
         "Permanent Seminar on Music Technology. Graduate Program in Music UNAM. CDMX. Coordinator. Semester 2023-I."
@@ -96,10 +96,10 @@ export const bioContent = {
       ],
       publications: "Publications",
       publicationsList: [
-        "Panorama. Writing of free and immersive spaces for audiovisual performance in <a href='https://www.repositorio.fam.unam.mx/handle/123456789/139'>Algoritmos Arruinados. Perspectivas situadas de tecnología musical.</a> Written by E. Ocelotl, D. Sotomayor and M. Teixido. Published by UNAM. 2022",
+        "Panorama. Writing of free and immersive spaces for audiovisual performance in <a href='https://www.repositorio.fam.unam.mx/handle/123456789/139'>Algoritmos Arruinados. Perspectivas situadas de tecnología musical.</a> Written by E. Ocelotl, D. Sotomayor and M. Teixido. Published by UNAM. 2022.",
         "<a href='https://zenodo.org/record/3715410#.X_4RjXWYVhE'>Aproximaciones al término Electroacústica en México.</a> Presented at the Electroacoustic Music Studies Network (EMS). Franco, A., Dávila, R., and Ocelotl, E. Mexico City.",
-        "<a href='https://zenodo.org/records/4091446'>Bellacode: localized textual interfaces for live coding music.</a> CO-AUTHOR. Madrid, Spain. January 2019. Accepted and published in the proceedings of the International Live Coding Conference. Written by L. N. del Ángel, D. Ogborn, E. Ocelotl and M. Teixido",
-        "<a href='https://dj.dancecult.net/index.php/dancecult/article/view/1066'>Saborítmico: A report from the dance floor in Mexico.</a> CO-AUTHOR. Online. November 2018. Accepted and published in Dancecult: Journal of Electronic Dance Music Culture. Article DOI: <a href='http://dx.doi.org/10.12801/1947-5403.2018.10.01.11'>doi</a>. Written by E. Ocelotl, M. Teixido and L. N. del Ángel. February 2021",
+        "<a href='https://zenodo.org/records/4091446'>Bellacode: localized textual interfaces for live coding music.</a> CO-AUTHOR. Madrid, Spain. January 2019. Accepted and published in the proceedings of the International Live Coding Conference. Written by L. N. del Ángel, D. Ogborn, E. Ocelotl and M. Teixido.",
+        "<a href='https://dj.dancecult.net/index.php/dancecult/article/view/1066'>Saborítmico: A report from the dance floor in Mexico.</a> CO-AUTHOR. Online. November 2018. Accepted and published in Dancecult: Journal of Electronic Dance Music Culture. Article DOI: <a href='http://dx.doi.org/10.12801/1947-5403.2018.10.01.11'>doi</a>. Written by E. Ocelotl, M. Teixido and L. N. del Ángel. February 2021.",
         "<a href='https://drive.google.com/file/d/1haihAd45sRI_Ef_XZYsDz1GKORvjNANx/view'>Pull push. Distributed and non-linear workflows in the performance, production and research of interactive audiovisual systems.</a> Manizales, Colombia. AUTHOR. May 2018. Accepted and published in the proceedings of the International Image Festival."
       ],
       backButton: "BACK"
